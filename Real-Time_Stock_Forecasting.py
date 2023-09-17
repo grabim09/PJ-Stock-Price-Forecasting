a10ad0ae219acb3bd8b16ff86edb6d22c45ead41
grabim09 <68747431+grabim09@users.noreply.github.com>
2023-09-18 01:20:45 +0700

    Update Real-Time_Stock_Forecasting.py

--- Real-Time_Stock_Forecasting.py
+++ Real-Time_Stock_Forecasting.py
@@ -36,7 +36,7 @@ tickers_name = tickers_data.Security.to_list()
 
 tuples = [(idx,(sym, name)) for idx, (sym, name) in enumerate(zip(tickers_symbol, tickers_name))]
 tickers = dict(tuples)
-tickers
+# tickers
 
 
 # In[18]:
@@ -45,7 +45,7 @@ tickers
 TC = pd.DataFrame(tickers).T
 TC.columns = ['Symbol', 'Name']
 TC['Symbol-Name'] = TC['Symbol'] + '-' + TC['Name']
-TC
+# TC
 
 
 # In[19]:
