ef326cf414d03147dfb4bcd29e83c1ca1d1da931
grabim09 <68747431+grabim09@users.noreply.github.com>
2023-09-18 01:19:48 +0700

    Update Real-Time_Stock_Forecasting.py

--- Real-Time_Stock_Forecasting.py
+++ Real-Time_Stock_Forecasting.py
@@ -18,32 +18,49 @@ st.title('Real-Time Stock Price Forecasting')
 # In[3]:
 
 
-import yfinance as yf
-
-
-# In[4]:
-
-
 # Read and print the stock tickers that make up S&P500
-tickers = pd.read_html(
+tickers_data = pd.read_html(
     'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
-print(tickers.head())
-
-
-# In[5]:
-
-
-tickers_list = tickers.Symbol.to_list()
-tickers_name = tickers.Security.to_list()
+print(tickers_data.head())
 
 
 # In[6]:
 
 
-chosen_ticker = st.selectbox("Please select available ticker below!",tickers_name)
+tickers_symbol = tickers_data.Symbol.to_list()
+tickers_name = tickers_data.Security.to_list()
 
 
-# In[7]:
+# In[11]:
+
+
+tuples = [(idx,(sym, name)) for idx, (sym, name) in enumerate(zip(tickers_symbol, tickers_name))]
+tickers = dict(tuples)
+tickers
+
+
+# In[18]:
+
+
+TC = pd.DataFrame(tickers).T
+TC.columns = ['Symbol', 'Name']
+TC['Symbol-Name'] = TC['Symbol'] + '-' + TC['Name']
+TC
+
+
+# In[19]:
+
+
+chosen_ticker = st.selectbox("Please select available ticker below!",TC['Symbol-Name'])
+
+
+# In[20]:
+
+
+import yfinance as yf
+
+
+# In[21]:
 
 
 data = yf.download(tickers='GOOGL',period='7d',interval='5m')
